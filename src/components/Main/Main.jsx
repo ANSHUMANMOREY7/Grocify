6382c919709e8d105d344484ba29f506efce83e9
anshuman morey <anshumanmorey2@gmail.com>
2025-11-04 22:13:19 +0530

    main image and main content added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import grocery from '../../assets/grocery.png';
 const Main = () => {
-  return (
-    <section>
-        <img src={grocery} alt="main image" />
-    </section>
-  )
+    return (
+        <section>
+            <div className='max-w-[1400px] mx-auto px-10 '>
+                {/* main content */}
+                <div >
+                    <span className='text-orange-500 bg-orange-100 px-5 py-2 rounded-full text-lg'>Export Best Quality...</span>
+                    <h1 className='text-7xl font-bold'>
+                        Tasty Organic <span className='text-orange-500'>Fruits</span> & <span className='text-orange-500'>Veggies</span> In Your City
+                    </h1>
+                </div>
+                {/* main image */}
+                <img src={grocery} alt="main image" />
+
+            </div>
+        </section>
+    )
 }
 
-export default Main ;
+export default Main;
