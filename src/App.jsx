cebe4a3caa559f160be636132a0d3e29f6ee36e1
anshuman morey <anshumanmorey2@gmail.com>
2025-11-04 19:39:28 +0530

    folder named components created which has child named Home and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react"
-
+import React from 'react'
+import Home from './components/Home/home'
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <div> <Home/></div>
+      
     </div>
   )
 }
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../Navbar/navbar';
+const Home = () => {
+  return (
+    <div>
+    <div> <Navbar/></div>      
+    </div>
+  )
+}
+
+export default Home
