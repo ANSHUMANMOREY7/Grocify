0a3f4e7a757500f0ad2b76baf5cf10979918724c
anshuman morey <anshumanmorey2@gmail.com>
2025-11-04 19:47:16 +0530

    nav links added

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -3,10 +3,25 @@ import React from 'react'
 const navbar = () => {
   return (
    <header>
-    <nav>
+    <nav  className='flex'>
       <a href="#">
         Gr<span>o</span>cify
       </a>
+      {/* menu */}
+      <ul  className='flex'>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">process</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
     </nav>
    </header>
   )
