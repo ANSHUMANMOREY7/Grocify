eba7e779dcc7f029472c56fe04c0e09024fbd7ab
anshuman morey <anshumanmorey2@gmail.com>
2025-11-08 21:30:09 +0530

    mobile view added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,14 +4,14 @@ import Button from '../Button/Button';
 const Main = () => {
     return (
         <section>
-            <div className='max-w-[1300px] mx-auto px-10 flex items-center pt-25  min-h-screen'>
+            <div className='max-w-[1300px] mx-auto px-10 flex items-center md:pt-25 pt-31 min-h-screen md:flex-row flex-col'>
                 {/* main content */}
                 <div className='flex-1'>
                     <span className='text-orange-500 bg-orange-100 px-5 py-2 rounded-full text-lg'>Export Best Quality...</span>
-                    <h1 className='text-7xl/20 font-bold mt-4 '>
+                    <h1 className='md:text-7xl/20 text-4xl/13 font-bold mt-4 '>
                         Tasty Organic <span className='text-orange-500'>Fruits</span> & <span className='text-orange-500'>Veggies</span> <br />In  Your City
                     </h1>
-                    <p className='text-zinc-600 text-lg max-w-[550px] mt-4 mb-5'>
+                    <p className='text-zinc-600 md:text-lg text-md max-w-[550px] mt-4 mb-5'>
                         Bred for a high content of beneficial substances. Our products are all fresh and healthy.
                     </p>
                     <Button content="Shop Now"/>
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { GoHeartFill } from "react-icons/go";
 import { HiMiniShoppingBag } from "react-icons/hi2";
 import { IoIosSearch } from "react-icons/io";
+import { TbMenu2 } from "react-icons/tb";
+
 
 const navbar = () => {
   return (
     <header className='bg-white fixed top-0 right-0 left-0'>
-      <nav className=' max-w-[1300px] mx-auto px-10 h-[14vh] flex justify-between items-center '>
+      <nav className=' max-w-[1300px] mx-auto px-10 md:h-[14vh] h-[12vh] flex justify-between items-center '>
         {/* nav logo */}
         <a href="#" className=' text-3xl font-bold'>
           Gr<span className='text-orange-500 uppercase'>o</span>cify
         </a>
         {/* menu */}
-        <ul className='flex gap-5'>
+        <ul className='gap-x-12 md:flex hidden items-center '>
           <li>
             <a href="#" className='font-semibold tracking-wider text-orange-500 hover:text-orange-500 text-lg'>Home</a>
           </li>
@@ -28,10 +30,11 @@ const navbar = () => {
         </ul>
         {/* nav actions */}
         <div className=' flex items-center gap-x-3'>
+          
           {/* search bar */}
-          <div className='flex p-0.5 items-center border-2 rounded-full border-orange-300'>
+          <div className=' md:flex hidden p-0.5 items-center border-2 rounded-full border-orange-300'>
             <input type="text" name='text' id='text' placeholder='Search...' autoComplete='off' className='outline-none border-none flex-1 h-[5vh] px-3 focus:outline-none' />
-            <button className=' text-2xl w-10 h-10 flex justify-center items-center bg-gradient-to-b from-orange-400 to-orange-600 rounded-full text-white '><IoIosSearch /></button>
+            <button className=' text-2xl w-10 h-10 flex justify-center items-center bg-linear-to-b from-orange-400 to-orange-600 rounded-full text-white '><IoIosSearch /></button>
           </div>
 
           <a href="#" className='text-zinc-800 hover:text-orange-500 text-2xl'>
@@ -40,6 +43,27 @@ const navbar = () => {
           <a href="#" className='text-zinc-800 hover:text-orange-500 text-2xl'>
             <HiMiniShoppingBag />
           </a>
+          {/* hamburger */}
+          <a href="#" className='text-zinc-800 text-3xl md:hidden '> <TbMenu2 /></a>
+          {/* mobile menu */}
+          <ul className='flex flex-col items-center justify-center gap-y-6 md:hidden fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500/50 backdrop-blur-xl p-8 w-66 h-[60vh]  z-50'>
+            <li>
+              <a href="#" className='font-semibold tracking-wider text-white hover:text-orange-200 text-lg'>Home</a>
+            </li>
+            <li>
+              <a href="#" className='font-semibold tracking-wider text-white hover:text-orange-200 text-lg'>About Us</a>
+            </li>
+            <li>
+              <a href="#" className='font-semibold tracking-wider text-white hover:text-orange-200 text-lg'>process</a>
+            </li>
+            <li>
+              <a href="#" className='font-semibold tracking-wider text-white hover:text-orange-200 text-lg'>Contact Us</a>
+            </li>
+            <li className='flex p-1 items-center border-2 rounded-full border-orange-300 m-1 bg-white/10'>
+              <input type="text" name='text' id='text' placeholder='Search...' autoComplete='off' className='outline-none border-none flex-1 h-[4vh] px-2 focus:outline-none bg-transparent text-white placeholder-white/80 w-39' />
+              <button className=' text-2xl w-10 h-10 flex justify-center items-center bg-linear-to-b from-orange-400 to-orange-600 rounded-full text-white '><IoIosSearch /></button>
+            </li>
+          </ul>
 
         </div>
       </nav>
