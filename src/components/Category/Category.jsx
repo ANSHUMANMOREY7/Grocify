5a4bc3b2aef7202ec10335fb40e9bf333e0d81f9
anshuman morey <anshumanmorey2@gmail.com>
2025-11-09 19:13:25 +0530

    category section placed in corrrect place and designd

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,14 +1,62 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
+import FruitCat from '../../assets/fruits-and-veggies.png'
+import SeaFoodCat from '../../assets/meat-and-seafood.png'
+import DairyCat from '../../assets/dairy-and-eggs.png'
+import Button from '../Button/Button'
 
 const Category = () => {
+  
+    const RenderCards = category.map(cards => {
+    return (
+      <div>
+           <div>
+            <img src={cards.image} />
+           </div>
+           <div>
+            <h2>{cards.title}</h2>
+           </div>
+           <div>
+            <p>{cards.description}</p>
+           </div>
+           <Button content="See All"/>
+      </div>
+    )
+  });
+
   return (
     <section>
       <div className='py-20 '>
         <Heading highlight='Shop' content='By Category'/>
+
+        {/* category cards */}
+        <div>
+          {RenderCards}
+        </div>
       </div>
     </section>
   )
 }
 
 export default Category
+
+const category = [
+  {
+    id: 1,
+    title: 'Fruits  & Veggies',
+    description: 'Fresh, organic produce sourced daily from local farms. Explore a wide range of seasonal fruits and crisp vegetables.',
+    image: FruitCat
+  },
+  {
+    id: 2,
+    title: 'Dairy & Eggs',
+    description: 'Wholesome dairy products and free-range eggs. From creamy milk and yogurt to artisanal cheeses.',
+    image: DairyCat
+  },
+  {
+    id: 3,
+    title: 'Meat & SeaFood',
+    description: 'High-quality, responsibly sourced meat and seafood. Choose from fresh cuts, marinated options, and more.',
+    image: SeaFoodCat
+  }
+];
--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,39 +1,21 @@
 import React from 'react'
-import FruitCat from '../../assets/fruits-and-veggies.png'
-import SeaFoodCat from '../../assets/meat-and-seafood.png'
-import DairyCat from '../../assets/dairy-and-eggs.png'
+
+
+
+
 const Heading = (props) => {
   return (
     <div>
       <div className='w-fit mx-auto'>
-          <h2 className='font-bold md:text-5xl text-[2.1rem]'>
-            <span className='text-orange-500 font-bold'> {props.highlight}</span> {props.content}
-          </h2>
-          <div className='w-34 h-1 bg-orange-300 md:mt-6 mt-3 ml-auto '></div>
-        </div>
+        <h2 className='font-bold md:text-5xl text-[2.1rem]'>
+          <span className='text-orange-500 font-bold'> {props.highlight}</span> {props.content}
+        </h2>
+        <div className='w-34 h-1 bg-orange-300 md:mt-6 mt-3 ml-auto '></div>
+      </div>
     </div>
   )
 }
 
-export default Heading 
+export default Heading
 
 
-const Category =[
-  { id:1 ,
-    title:'Fruits  & Veggies',
-    description:'Fresh, organic produce sourced daily from local farms. Explore a wide range of seasonal fruits and crisp vegetables.',
-    image:FruitCat
-  },
-  {
-    id:2,
-    title:'Dairy & Eggs',
-    description:'Wholesome dairy products and free-range eggs. From creamy milk and yogurt to artisanal cheeses.',
-    image:DairyCat
-  },
-  {
-    id:3,
-    title:'Meat & SeaFood',
-    description:'High-quality, responsibly sourced meat and seafood. Choose from fresh cuts, marinated options, and more.',
-    image:SeaFoodCat
-  }
-];
