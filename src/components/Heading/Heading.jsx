0c55c839cd9bd7c356cf240e7b46fdff39bbc19a
anshuman morey <anshumanmorey2@gmail.com>
2025-11-09 18:41:08 +0530

    category array added to render category cards

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import FruitCat from '../../assets/fruits-and-veggies.png'
+import SeaFoodCat from '../../assets/meat-and-seafood.png'
+import DairyCat from '../../assets/dairy-and-eggs.png'
 const Heading = (props) => {
   return (
     <div>
@@ -13,4 +15,25 @@ const Heading = (props) => {
   )
 }
 
-export default Heading
+export default Heading 
+
+
+const Category =[
+  { id:1 ,
+    title:'Fruits  & Veggies',
+    description:'Fresh, organic produce sourced daily from local farms. Explore a wide range of seasonal fruits and crisp vegetables.',
+    image:FruitCat
+  },
+  {
+    id:2,
+    title:'Dairy & Eggs',
+    description:'Wholesome dairy products and free-range eggs. From creamy milk and yogurt to artisanal cheeses.',
+    image:DairyCat
+  },
+  {
+    id:3,
+    title:'Meat & SeaFood',
+    description:'High-quality, responsibly sourced meat and seafood. Choose from fresh cuts, marinated options, and more.',
+    image:SeaFoodCat
+  }
+];
