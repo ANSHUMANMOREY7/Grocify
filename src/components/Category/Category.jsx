38f24a4b22b924c7c5923468c2c86621cdf1aa41
anshuman morey <anshumanmorey2@gmail.com>
2025-11-09 17:31:39 +0530

    heading section seperated from category section

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
+import Heading from '../Heading/Heading'
 
 const Category = () => {
   return (
     <section>
       <div className='py-20 '>
-        <div className='w-fit mx-auto'>
-          <h2 className='font-bold md:text-5xl text-[2.1rem]'>
-            <span className='text-orange-500 font-bold'> Shop</span> By Category
-          </h2>
-          <div className='w-34 h-1 bg-orange-300 md:mt-6 mt-3 ml-auto '></div>
-        </div>
-
+        <Heading />
       </div>
     </section>
   )
---
+++ src/components/Heading/Heading.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div>
+      <div className='w-fit mx-auto'>
+          <h2 className='font-bold md:text-5xl text-[2.1rem]'>
+            <span className='text-orange-500 font-bold'> Shop</span> By Category
+          </h2>
+          <div className='w-34 h-1 bg-orange-300 md:mt-6 mt-3 ml-auto '></div>
+        </div>
+    </div>
+  )
+}
+
+export default Heading
