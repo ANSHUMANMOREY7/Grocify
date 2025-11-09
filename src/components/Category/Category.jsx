61ab3d5b16acd053dcdc92d8a7c6751ccc622b82
anshuman morey <anshumanmorey2@gmail.com>
2025-11-09 19:19:48 +0530

    category section given fixed width

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -26,7 +26,7 @@ const Category = () => {
 
   return (
     <section>
-      <div className='py-20 '>
+      <div className='py-20 max-w-[1300px] mx-auto px-10'>
         <Heading highlight='Shop' content='By Category'/>
 
         {/* category cards */}
