38a4101b189d624b4ae7908415b15fd5d4b6cb20
anshuman morey <anshumanmorey2@gmail.com>
2025-11-10 22:46:24 +0530

    process section added

---
+++ src/components/Process/Process.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Process = () => {
+  return (
+    <div className='max-w-[1300px] mx-auto px-10'>
+        <Heading hightlight="Our" content="Process" />
+      
+    </div>
+  )
+}
+
+export default Process
