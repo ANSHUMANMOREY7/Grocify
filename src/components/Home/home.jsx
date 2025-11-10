6a255e1d76e282fe71f07ecdc801dfaca4a53211
anshuman morey <anshumanmorey2@gmail.com>
2025-11-10 20:22:49 +0530

    designing of products done

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Navbar/navbar';
 import Main from '../Main/Main';
 import Category from '../Category/Category';
 import Values from '../Values/Values';
+import Products from '../Products/Products';
 const Home = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const Home = () => {
       <Main/>  
       <Category/>
       <Values />
+      <Products />
     </div>
   )
 }
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Heading from '../Heading/Heading'
+
+const Products = () => {
+
+    const categories = ["All", "Fruits", "Vegetables ", "Dairy", "Seafood"];
+    const [ activeTab , setActiveTab] = useState("All");
+    return (
+        <section>
+            <div className='max-w-[1300px] mx-auto px-10 py-20'>
+                <Heading highlight="Our" content="Products" />
+
+                {/* Tabs  */}
+
+                <div className='flex justify-center gap-9 mt-10'>
+                    {categories.map(category => {
+                        return (
+                           <button key={category}
+                           className={`text-lg cursor-pointer px-5 py-2 rounded-lg
+                           ${activeTab === category ? 'bg-gradient-to-b from-orange-400 to-orange-500 text-white' : 'bg-zinc-300'}`} onClick={()=>setActiveTab(category)}>
+                            {category}
+                           </button>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Products
