41501ae18eb217eb4f39023ae774a346d0b423a0
anshuman morey <anshumanmorey2@gmail.com>
2025-11-10 22:42:30 +0530

    modifications added

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -6,7 +6,7 @@ const Discount = () => {
    <section className='relative bg-zinc-300/50'>
      {/* background image only on md and larger */}
      <div className='hidden md:block absolute inset-0 bg-right bg-contain bg-no-repeat' style={{backgroundImage:`url(${FreshFruits})`}} aria-hidden="true" />
-    <div className='bg-zinc-300/50 md:bg-transparent max-w-[1400px] mx-auto px-10 py-10 flex flex-col md:flex-row items-start md:items-center gap-x-4 gap-y-4'>
+    <div className='bg-zinc-300/50 md:bg-transparent max-w-[1400px] mx-auto px-10 py-10 flex flex-col md:flex-row items-start md:items-center gap-x-1 gap-y-4'>
       <span className='text-6xl md:text-9xl text-orange-500 font-bold inline-block transform rotate-0 md:rotate-90 md:shrink-0'>20%</span>
       <div className='max-w-[700px] flex-1'>
         <h3 className=' text-4xl md:text-7xl text-zinc-800 font-bold'>Fisrt Order Discount!</h3>
