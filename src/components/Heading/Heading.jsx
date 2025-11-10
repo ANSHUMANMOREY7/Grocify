919ce852499e1f40caaf25bd7c3817ddbc658539
anshuman morey <anshumanmorey2@gmail.com>
2025-11-10 23:41:44 +0530

    our process cards added

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -7,7 +7,7 @@ const Heading = (props) => {
   return (
     <div>
       <div className='w-fit mx-auto'>
-        <h2 className='font-bold md:text-5xl text-[2.1rem]'>
+        <h2 className='font-bold md:text-5xl text-[2.1rem] text-zinc-800'>
           <span className='text-orange-500 font-bold'> {props.highlight}</span> {props.content}
         </h2>
         <div className='w-34 h-1 bg-orange-300 md:mt-6 mt-3 ml-auto '></div>
--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -1,13 +1,67 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
+import { TbCircleNumber1Filled, TbCircleNumber2Filled, TbCircleNumber3Filled, TbCircleNumber4Filled } from "react-icons/tb";
+import { PiFactory, PiPlant } from "react-icons/pi";
+import { SlBadge } from "react-icons/sl";
+import { FiTruck } from "react-icons/fi";
 
 const Process = () => {
-  return (
-    <div className='max-w-[1300px] mx-auto px-10'>
-        <Heading hightlight="Our" content="Process" />
-      
-    </div>
-  )
+    const RenderSteps = steps.map(items => {
+        return (
+            <div className='bg-zinc-300/50 rounded-lg p-5'>
+                 <span className=' flex text-orange-500 w-18 text-7xl h-18 justify-center align-center rounded-full'>{items.number}</span>
+                 <div className='flex justify-center'>
+                    <span className='text-5xl  text-zinc-800 font-bold '>{items.icon}</span>
+                 </div>
+                 <div className='p-3 '>
+                    <h4 className='font-bold text-2xl text-zinc-800'>{items.title}</h4>  
+                    <p className='font-semibold text-zinc-600'>{items.para}</p>
+                 </div>
+            </div>
+
+        )
+    })
+    return (
+        <div className='max-w-[1300px] mx-auto px-10 py-20'>
+            <div className=''>
+                <Heading highlight="Our" content="Process" />
+            </div>
+            <div className='flex gap-10 mt-20 items-center'>
+               {RenderSteps}
+            </div>
+        </div>
+    )
 }
 
 export default Process
+
+const steps = [
+    {
+        id: 1,
+        number: <TbCircleNumber1Filled />,
+        title: "Sourcing",
+        para: "It is a long established fact that a reader",
+        icon: <PiPlant />,
+    },
+    {
+        id: 2,
+        number: <TbCircleNumber2Filled />,
+        title: "Manufacturing",
+        para: "It is a long established fact that a reader",
+        icon: <PiFactory />,
+    },
+    {
+        id: 3,
+        number: <TbCircleNumber3Filled />,
+        title: "Quality Control",
+        para: "It is a long established fact that a reader",
+        icon: <SlBadge />,
+    },
+    {
+        id: 4,
+        number: <TbCircleNumber4Filled />,
+        title: "Logistics",
+        para: "It is a long established fact that a reader",
+        icon: <FiTruck />
+    }
+];
