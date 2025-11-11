1fa2c3f7092502c424e2d421ceddf0f98772e89d
anshuman morey <anshumanmorey2@gmail.com>
2025-11-11 21:08:42 +0530

    Footer site logo added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-
+    <footer className='bg-zinc-300/50 py-20 '>
+       <div>
+        <a href="#" className=' text-3xl font-bold'>
+          Gr<span className='text-orange-500 uppercase'>o</span>cify
+        </a>
+       </div>
     </footer>
   )
 }
