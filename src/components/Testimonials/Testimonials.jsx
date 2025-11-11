31bf38501ff7ab1f39024dbff9e705bdc09033e0
anshuman morey <anshumanmorey2@gmail.com>
2025-11-11 21:03:13 +0530

    Footer page added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,7 +4,7 @@ import Heading from '../Heading/Heading'
 const Testimonials = () => {
   return (
    <section>
-    <div className='max-w-[1300px] px-10 mx-auto'>
+    <div className='max-w-[1300px] px-10 mx-auto py-20'>
        <Heading highlight="Customers" content="Saying"/>
     </div>
    </section>
