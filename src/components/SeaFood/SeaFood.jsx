134431d5b4b96c1e1b9efb6fc82fa1febf866d73
anshuman morey <anshumanmorey2@gmail.com>
2025-11-13 18:41:03 +0530

    seafood image imported and added in seafood banner

--- src/components/SeaFood/SeaFood.jsx
+++ src/components/SeaFood/SeaFood.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
 import CategoryPage from '../CategoryPage/CategoryPage'
+import BgSeaFood from '../../assets/seafood-banner.jpg'
 
 const SeaFood = () => {
   return (
     <div>
-      <CategoryPage title="  Meat & SeaFood" />
+      <CategoryPage title="Meat & SeaFood" bgImage={BgSeaFood}/>
     </div>
   )
 }
